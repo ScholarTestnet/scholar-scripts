8171a40ad59e15ea27a0a7c1804dc2177b65de28
Denis <carriere.denis@gmail.com>
2018-05-01 17:04:21 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mkdir -p logs;
-nohup nodeos --data-dir ./data-dir --config-dir ./config-dir  > ./logs/eos.log 2>&1 &
-echo $! > eos.pid
+nohup nodeos >./logs/nodeos.log 2>&1 &
+echo $! > nodeos.pid
+nohup ipfs daemon >./logs/ipfs.log 2>&1 &
+echo $! > ipfs.pid
